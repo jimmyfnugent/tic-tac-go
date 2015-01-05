1d38a898c8753dcc46d2efddcd66f95c0f630b9c
apmcleod <apmcleod@gmail.com>
2015-01-05 21:29:47 +0000

    Removed now unused layout xml

--- src/com/tictacgo/TicTacGoGame.java
+++ src/com/tictacgo/TicTacGoGame.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.Window;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.PopupWindow;
@@ -18,7 +17,6 @@ import com.tictacgo.data.Board;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 
 public class TicTacGoGame extends Activity {
   /**
@@ -69,7 +67,6 @@ public class TicTacGoGame extends Activity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    requestWindowFeature(Window.FEATURE_NO_TITLE);
     setContentView(R.layout.game);
 
     Intent intent = getIntent();
--- src/com/tictacgo/TicTacGoMenu.java
+++ src/com/tictacgo/TicTacGoMenu.java
@@ -1,34 +1,12 @@
 package com.tictacgo;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.Window;
-import android.view.animation.Animation;
-import android.view.animation.Animation.AnimationListener;
-import android.view.animation.RotateAnimation;
-import android.view.animation.TranslateAnimation;
-import android.widget.FrameLayout;
-import android.widget.FrameLayout.LayoutParams;
-import android.widget.ImageView;
-import android.widget.PopupWindow;
 import android.widget.RadioGroup;
-import android.widget.TableRow;
 import android.widget.TextView;
-
-import com.tictacgo.data.Board;
-import com.tictacgo.data.Board.Player;
-import com.tictacgo.data.Piece;
 
 
 public class TicTacGoMenu extends Activity {
@@ -37,7 +15,6 @@ public class TicTacGoMenu extends Activity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    requestWindowFeature(Window.FEATURE_NO_TITLE);
     setContentView(R.layout.menu);
 
     findViewById(R.id.playButton).setOnClickListener(new OnClickListener() {
